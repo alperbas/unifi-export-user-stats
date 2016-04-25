490d29231466e3f487c27e85a6eee0e5a1bee5c0
alperbas <alper.bassarac@gmail.com>
2016-04-25 11:25:46 +0300

    Dev (#1)
    * spacing
    
    * added mysqlupdate function
    
    * use mysqupdate function
    
    * update mysqlupdate func
    
    only update if AuthInfo.sessionid is null
    
    * fix mysqlcheck return value
    
    * add dummy return value to mysqlupdate func
    
    * add echo for test
    
    * update echo in mysqlcheck func
    
    * dumy
    
    * dum
    
    * dummy
    
    * dum
    
    * dumy
    
    * add fault check to mysqlput
    
    * missing comma
    
    * fix apos on query at check func
    
    * remove echo and update mysql connect func name
    
    * add descriptions and remove echo

--- mongo.php
+++ mongo.php
@@ -2,57 +2,76 @@
 
     require 'vendor/autoload.php';
 
-    function mysqlput($query) {
-        $dbservername = "ipaddr";
+    function mysqlconn($query) {
+        $dbservername = "193.192.100.78";
         $dbusername = "radius";
-        $dbpassword = "passwd";
+        $dbpassword = "Boss!*vLa34";
         $dbname = "radius";
         $connection = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
-        return $connection->query($query);
+        $result = $connection->query($query);
+        if (!$result) {
+            echo "Error executing query: (".$mysqli->errno.") ".$mysqli->error."\n";
+        } else {
+            return $result;
+        }
     }
 
     function mysqlcheck($field, $value) {
         $query = "SELECT $field FROM radacct WHERE $field = '$value'";
-	$result = mysqlput($query);
-	$row = mysqli_fetch_assoc($result);
-	#echo $row[$field]."\n";
+        $result = mysqlconn($query);
+        $row = mysqli_fetch_assoc($result);
+        #echo "field: ".$field." value: ".$value." row: ".$row[$field]."\n";
 
-	if(is_null($row[$field])) {
-	    return 1;
+        if(is_null($row[$field])) {
+            return 1;
         } else {
-	    return 0;
+            return 0;
         }
+    }
+
+    function mysqlupdate ($mac) {
+        $query = "UPDATE AuthInfo
+                LEFT JOIN radacct ON AuthInfo.macaddress = radacct.callingstationid
+                SET AuthInfo.sessionid = radacct.radacctid
+                WHERE AuthInfo.logintime > radacct.acctstarttime
+                AND AuthInfo.logintime < radacct.acctstoptime
+                AND AuthInfo.sessionid is null
+                AND radacct.callingstationid = '$mac';";
+        mysqlconn($query);
+        echo $mac." 'i update ettim.\n";
+        return 1;
     }
 
     function mongoget() {
         $client = new MongoDB\Client("mongodb://localhost:27117");
         $collection = $client->ace_stat->stat_archive;
-    
         #$result = $collection->find( [ 'mac' => '00:26:82:50:85:d4' ] );
+        # get all sessions from unifi
         $result = $collection->find();
-    
+
+        # for each sesion in unifi
         foreach ($result as $entry) {
-          $acctuniqueid = $entry['_id'];
-          $acctsessionid = $entry['session'];
-          $acctstarttime = $entry['assoc_time'];
-          $acctstoptime = $entry['disassoc_time'];
-          $acctsessiontime = $entry['duration'];
-          $acctinputoctets = $entry['rx_bytes'];
-          $acctoutputoctets = $entry['tx_bytes'];
-          $calledstationid = $entry['ap_mac'];
-          $callingstationid = $entry['mac'];
-          $framedipaddress = $entry['ip'];
-          
-	  $sqlquery = "INSERT INTO radacct (acctuniqueid, acctsessionid, acctstarttime, acctstoptime, acctsessiontime, acctinputoctets, acctoutputoctets, calledstationid, callingstationid, framedipaddress) VALUES ('$acctuniqueid', '$acctsessionid', FROM_UNIXTIME('$acctstarttime'), FROM_UNIXTIME('$acctstoptime'), '$acctsessiontime', '$acctinputoctets', '$acctoutputoctets', '$calledstationid', '$callingstationid', '$framedipaddress')";
+            $acctuniqueid = $entry['_id'];
+            $acctsessionid = $entry['session'];
+            $acctstarttime = $entry['assoc_time'];
+            $acctstoptime = $entry['disassoc_time'];
+            $acctsessiontime = $entry['duration'];
+            $acctinputoctets = $entry['rx_bytes'];
+            $acctoutputoctets = $entry['tx_bytes'];
+            $calledstationid = $entry['ap_mac'];
+            $callingstationid = $entry['mac'];
+            $framedipaddress = $entry['ip'];
 
-	  if(mysqlcheck('acctuniqueid', $acctuniqueid)) {
-		echo "null\n";
-		mysqlput($sqlquery);
-	  } else {
-	  	echo "notNull\n";
-	  }
+            $insertquery = "INSERT INTO radacct (acctuniqueid, acctsessionid, acctstarttime, acctstoptime, acctsessiontime, acctinputoctets, acctoutputoctets, calledstationid, callingstationid, framedipaddress)
+                VALUES ('$acctuniqueid', '$acctsessionid', FROM_UNIXTIME('$acctstarttime'), FROM_UNIXTIME('$acctstoptime'), '$acctsessiontime', '$acctinputoctets', '$acctoutputoctets', '$calledstationid', '$callingstationid', '$framedipaddress')";
 
-	  #echo $acctuniqueid, ' : ', $acctsessionid, ' ', $acctsessiontime, "\n";
+            # check if sesion exists in radius db
+            if(mysqlcheck('acctuniqueid', $acctuniqueid)) {
+                # Insert session to radius.radacct table.
+                mysqlconn($insertquery);
+                # Update sessionID for captive portal login radius.AuthInfo table.
+                mysqlupdate($callingstationid);
+            }
         }
     }
 
